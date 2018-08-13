967511de326c08ab6cfb67a98e04167606c6c5d7
yinran100 <1005469156@qq.com>
2018-08-13 15:15:42 +0800

    增加滚动轮播部分

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/**
+  1.meta标签中content属性中加入minium-scale=1.0,maximun-scale=1.0,user-scalable=no可以防止手指放大缩小页面 
+  2.为了统一不同浏览器的默认css属性，引入基础样式修饰reset.css
+  3.为了解决多倍屏里面1像素边框会被显示多像素的问题需要引入border.css 
+  4.在移动端开发中某些设备或浏览器上当时有click事件时会有300ms的延迟。解决方案引入fastclick库 npm install fastclick --save (--save表示开发和线上打包环境都会包含)
+  stylus版本0.52.4  stylus-loader版本2.5.0，否则vue 报错：Module build failed: Error: Cannot find module 'when'
+ swiper轮播插件需要npm安装 npm install vue-awesome-swiper@2.6.7 --save   （防止最新版本有些bug，采用旧版本2.6.7）
+ 在网络不通畅的移动端，加载图片时会有延迟，目标元素需要占用该位置的宽高,给个灰色背景颜色
+ * 
+ */
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'style/reset.css'
 import 'style/border.css'
 import 'style/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */ )
 /* eslint-disable no-new */
 var vm = new Vue({
   el: '#app',
---
+++ src/page/home/components/Swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption"  >
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id" >
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  //props: ["swiperOption"],
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+        autoplay: 5000
+      },
+      swiperList: [
+        {
+          id: "001",
+          imgUrl:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180716_wap_11043.jpg"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active // css样式穿透
+  background-color: #fff
+
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 24.53%
+
+  .swiper-img
+    width: 100%
+</style>
+
