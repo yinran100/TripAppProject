967270695c2504238672d1e82720a1cfd86ff546
yinran100 <1005469156@qq.com>
2018-08-14 23:10:29 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
   2.为了统一不同浏览器的默认css属性，引入基础样式修饰reset.css
   3.为了解决多倍屏里面1像素边框会被显示多像素的问题需要引入border.css 
   4.在移动端开发中某些设备或浏览器上当时有click事件时会有300ms的延迟。解决方案引入fastclick库 npm install fastclick --save (--save表示开发和线上打包环境都会包含)
-  stylus版本0.52.4  stylus-loader版本2.5.0，否则vue 报错：Module build failed: Error: Cannot find module 'when'
+  stylus版本0.52.4  stylus-loader版本2.5.0，npm install stylus@0.52.4 -g --save,npm install stylus-loader@2.5.0 -g --save  否则vue 报错：Module build failed: Error: Cannot find module 'when' 
   swiper轮播插件  需要npm安装 npm install vue-awesome-swiper@2.6.7 --save   （防止最新版本有些bug，采用旧版本2.6.7）
   axios用于数据Ajax请求  npm install axios --save
  在网络不通畅的移动端，加载图片时会有延迟，目标元素需要占用该位置的宽高,给个灰色背景颜色
@@ -21,6 +21,7 @@ import 'style/reset.css'
 import 'style/border.css'
 import 'style/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
